0274b9767608b12cef28493913c47eabdb4b3188
Piotr Matras <piotrmatras21@gmail.com>
2021-09-27 18:56:02 +0200

    switched to value defined in 'config' object

--- encryption_server/database/db.js
+++ encryption_server/database/db.js
@@ -2,13 +2,15 @@ const Loki = require('lokijs');
 const getMockedUsers = require('../config/users');
 
 const DB_NAME = 'encryption_server';
-const USERS_COLLECTION = 'users';
+const {
+  db: { usersCollection },
+} = require('../config/config');
 
 const db = new Loki(DB_NAME);
 
 (async () => {
   const mockedUsers = db
-    .addCollection(USERS_COLLECTION, { unique: 'email' })
+    .addCollection(usersCollection, { unique: 'email' })
     .insert(await getMockedUsers());
   console.log(
     `Database populated with users: ${JSON.stringify(mockedUsers, null, 2)}`
