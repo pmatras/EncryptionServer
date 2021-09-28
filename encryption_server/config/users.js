6e59afbd5b6267f7a71a525b78d20cf2da9d7836
Piotr Matras <piotrmatras21@gmail.com>
2021-09-28 11:34:35 +0200

    added 'id' prop for every user

--- encryption_server/config/users.js
+++ encryption_server/config/users.js
@@ -2,12 +2,14 @@ const { hashPassword } = require('../helpers/password');
 
 const getMockedUsers = async () => [
   {
+    id: 0,
     name: 'John Doe',
     username: 'john',
     email: 'john@example.com',
     password: await hashPassword('john123'),
   },
   {
+    id: 1,
     name: 'Jane Doe',
     username: 'jane',
     email: 'jane@example.com',
