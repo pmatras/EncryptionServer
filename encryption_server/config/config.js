7bcdb36ec7ddec21f199f5643b0f22ea0500f305
Piotr Matras <piotrmatras21@gmail.com>
2021-09-28 11:34:06 +0200

    added example .env file, added new properties to config object

--- encryption_server/config/config.js
+++ encryption_server/config/config.js
@@ -2,6 +2,7 @@ const config = {
   db: {
     name: 'encryption_server',
     usersCollection: 'users',
+    rsaKeysCollection: 'rsa_keys',
   },
   server: {
     port: process.env.SERVER_PORT,
@@ -11,6 +12,10 @@ const config = {
     key: process.env.JWT_TOKEN_KEY,
     algorithm: 'HS256',
     expirationSecs: 5 * 60,
+  },
+  rsaKeys: {
+    keysLength: 2048,
+    privateKeyPassword: process.env.PRIVATE_RSA_KEY_PASSWORD,
   },
 };
 
